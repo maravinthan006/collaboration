93646eb191a6cf7fe36ca2d4b7e95c3966a70806
maravinthan006 <maravinthan006@gmail.com>
2017-07-10 12:41:27 +0530

    collab front end

---
+++ _user/userservice.js
@@ -0,0 +1,25 @@
+app.factory('UserService',function($http){
+	var userService={}
+	var BASE_URL="http://localhost:8085/collabotrationBackEnd"
+	
+	userService.registerUser=function(user){
+		return $http.post(BASE_URL+"/register",user);
+	}
+	
+	userService.login=function(user){
+		return $http.post(BASE_URL + "/login",user)
+	}
+	
+	userService.logout=function(){
+		return $http.put(BASE_URL + "/logout");
+	}
+	
+	userService.getUser=function(){
+		return $http.get(BASE_URL + "/getuser")
+	}
+	
+	userService.updateUser=function(user){
+		return $http.put(BASE_URL + "/updateuser",user)
+	}
+	return userService;
+})
